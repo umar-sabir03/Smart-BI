a80f812a8b89bd0655f4a5cde24f6f9598ffdff7
PiLog <umar.sabir@piloggroup.com>
2024-01-29 15:12:24 +0530

    Optimized imports

--- src/main/java/com/pilog/mdm/config/SecurityConfig.java
+++ src/main/java/com/pilog/mdm/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.pilog.mdm.config;
 
 import com.pilog.mdm.filters.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
